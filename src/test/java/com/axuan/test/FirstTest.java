be5ca9bdc9ea852f02d5cc3aa02d9b5913eddb8b
Moon_xuan <1694936091@qq.com>
2022-05-15 15:35:50 +0800

    update

--- src/test/java/com/axuan/test/FirstTest.java
+++ src/test/java/com/axuan/test/FirstTest.java
@@ -1,8 +1,14 @@
 package com.axuan.test;
 
+import org.junit.Test;
+
 /**
  * @author axuan
  */
 public class FirstTest {
 
+  @Test
+  public void test() {
+    System.out.println("test");
+  }
 }
